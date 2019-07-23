60dbb251e19f5dd49095dc96b3845c9c6bf9338f
Sam Stites <stites@users.noreply.github.com>
2019-07-23 14:16:40 -0400

    fix shell.nix

--- shell.nix
+++ shell.nix
@@ -3,30 +3,41 @@
 with pkgs;
 
 let
-  pkgs-pytorch_41 = import ./nix/pin/nixpkgs-pytorch_04.nix { };
+  pkgs-pytorch_041 = import ./nix/pin/nixpkgs-pytorch_04.nix { };
+
   developmentPython36 =
     let
       mypython = pkgs.python36.override {
-        packageOverrides = self: super: let
-          pytorch_11 = (pkgs.callPackage ./nix/pytorch/release.nix { inherit pkgs; pythonPackages = pkgs.python36Packages; }).pytorchWithCuda10;
-          pytorch = pkgs-pytorch_41.python36Packages.pytorchWithCuda;
-          torchvision = pkgs.python36Packages.torchvision.override { inherit pytorch; };
+        packageOverrides = self: super: rec {
+          numpy_041       = pkgs-pytorch_041.python36Packages.numpy;
+          pytorch_041     = pkgs-pytorch_041.python36Packages.pytorchWithCuda;
+          torchvision_041 = pkgs-pytorch_041.python36Packages.torchvision.override { pytorch = pytorch_041; };
 
-          probtorch = pkgs.callPackage ./nix/probtorch { inherit (pkgs.python36Packages) buildPythonPackage; inherit pytorch; };
-          flatdict = pkgs.callPackage ./nix/flatdict.nix { inherit (pkgs.python36Packages) buildPythonPackage fetchPypi; };
-          pygtrie = pkgs.callPackage ./nix/pygtrie.nix { inherit (pkgs.python36Packages) buildPythonPackage fetchPypi; };
-          matplotlib = pkgs.python36Packages.matplotlib.override { enableQt = true; };
+          pytorch_110     = (pkgs.callPackage ./nix/pytorch/release.nix {
+            inherit pkgs; pythonPackages = pkgs.python36Packages;
+          }).pytorchWithCuda10;
+
+          probtorch   = pkgs.callPackage ./nix/probtorch    { inherit (pkgs.python36Packages) buildPythonPackage; pytorch = pytorch_041; };
+          flatdict    = pkgs.callPackage ./nix/flatdict.nix { inherit (pkgs.python36Packages) buildPythonPackage fetchPypi; };
+          pygtrie     = pkgs.callPackage ./nix/pygtrie.nix  { inherit (pkgs.python36Packages) buildPythonPackage fetchPypi; };
+          matplotlib  = pkgs.python36Packages.matplotlib.override { enableQt = true; numpy = numpy_041; };
           combinators = pkgs.callPackage ./. {
             inherit (pkgs.python36Packages) buildPythonPackage;
             inherit probtorch flatdict pygtrie matplotlib;
           };
-        in { inherit pytorch probtorch combinators; };
+        };
         self = mypython;
       };
-    in mypython.withPackages(ps: with ps; [ combinators ipywidgets ipython jupyter notebook bokeh torchvision ]);
+
+    in mypython.withPackages(ps: with ps; [
+      combinators ipywidgets ipython jupyter notebook torchvision_041 pytorch_041 scipy imageio pillow
+    ]); # bokeh is brokehn
 in
 
-developmentPython36.env
+mkShell {
+  # numpy_041 doesn't get added properly from withPackages
+  buildInputs = [ developmentPython36 pkgs-pytorch_041.python36Packages.pytorchWithCuda pkgs-pytorch_041.python36Packages.numpy ];
+}
 
 
 
